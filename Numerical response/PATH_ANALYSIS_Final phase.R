dda5fee5402b37b87617dfe41db3a6e5478128bd
Your Name <nicolas@MacBook-Air-de-Nicolas-2.local>
2018-01-18 11:22:34 -0500

    MAJ script et nouveaux modèles à partir du dernier sélectionné (M3f6) Nouvelle base de travail avec deux nouveaux modèles valides.

--- Numerical response/PATH_ANALYSIS_Final phase.R
+++ Numerical response/PATH_ANALYSIS_Final phase.R
@@ -3,14 +3,29 @@ rm(list = ls()) #clean R memory
 setwd(dir = "/Users/nicolas/OneDrive - Université de Moncton/Doc doc doc/Ph.D. - ANALYSES/R analysis/Data")
 
 
-# COMPARER F ET F1 AVANT DE FAIRE N'IMPORTE QUOI D'AUTRE !!!!!!!!!!
+#f <- read.table("Path analysis_data 3.txt", sep = ",", dec = ".", h = T)
+#f1 <- read.table("Path analysis_data 3bis.txt", sep = ",", dec = ".", h = T)
 
+#s1 <- summary(f)
+#s2 <- summary(f1)
 
-f <- read.table("Path analysis_data 3.txt", sep = ",", dec = ".", h = T)
-f1 <- read.table("Path analysis_data 3bis.txt", sep = ",", dec = ".", h = T)
+# Les deux jeux de donnees ont ete compares et sont identiques. A partir de là, utilisation de "Path analysis_data 3bis" car plus complet en terme de variables : les variables prop_f0x_dens, sumAO et AOnidif ont ete rajoutees
+rm(list = ls()) #clean R memory
+f <- read.table("Path analysis_data 3bis.txt", sep = ",", dec = ".", h = T)
 
-summary(f)
-summary(f1)
+# Premiere etape : retrait des variables inutiles pour les analyses de piste et qui pourraient engendrer des NAs (et donc de la perte de donnees) inutilement
+# Retrait de : X, NO, HABITAT, INI, INICOD, ECLO, EXPO, PRED, UTM.E, UTM.N, NB_VIS, NEST.TYPE, FIN, SD_TEMP, SD_prec, prim_prod, nest_density, clutch_size, egg_abun, ratio_JUVad, brood_size, nest_succ
+
+# BDD a utiliser si analyses avec lmg_C2 ou lmg_C1_C2
+mC2 <- f[,-c(1, 3:8, 10:14, 16, 18, 25, 35, 42:47 )]
+mC2 <- na.omit(mC2)
+summary(mC2)
+pairs(mC2)
+
+# BDD a utiliser si analyses avec lmg_C1
+mC1 <- f[,-c(1, 3:8, 10:14, 16, 18, 25, 30, 32, 34, 35, 42:47 )]
+summary(mC1)
+
 
 #Package nécessaires
 require(nlme)
@@ -18,74 +33,110 @@ require(lme4)
 require(piecewiseSEM)
 require(ggm)
 
+# Fonction pour impression coef correlation et p.value sur les pair.panels avec variables utilisees dans analyses de piste
+panel.cor <- function(x, y, digits = 2, cex.cor, ...)
+{
+  usr <- par("usr"); on.exit(par(usr))
+  par(usr = c(0, 1, 0, 1))
+  # correlation coefficient
+  r <- cor(x, y)
+  txt <- format(c(r, 0.123456789), digits = digits)[1]
+  txt <- paste("r= ", txt, sep = "")
+  text(0.5, 0.6, txt)
+  
+  # p-value calculation
+  p <- cor.test(x, y)$p.value
+  txt2 <- format(c(p, 0.123456789), digits = digits)[1]
+  txt2 <- paste("p= ", txt2, sep = "")
+  if(p<0.01) txt2 <- paste("p= ", "<0.01", sep = "")
+  text(0.5, 0.4, txt2)
+}
+
+
 #### Path analysis with Path analysis_data 3 #####
-nest<-na.omit(f)
-M3f6<-list(
-  lm(fox_dens~lmg_abun+winAO+cumul_prec+MEAN_temp, data = f),
-  glmer(SN~fox_dens+cumul_prec+MEAN_temp+ (1|AN), data=f, family=binomial(link = "logit")),
-  lm(lmg_abun~winAO+MEAN_temp+cumul_prec, data = f))
+#i.e. lmg_C2 et fox_dens avec le jeu de donnees mC2
+
+#Exploration visuelle des donnees
+X11()
+pairs(mC2[,c(15, 17, 20, 12, 4, 26)], upper.panel = panel.cor)
+
+#Modele de piste
+M3f6 <- list(
+  lm(fox_dens ~ lmg_C2 + winAO + cumul_prec + MEAN_temp, data = mC2),
+  glmer(SN ~ fox_dens + cumul_prec + MEAN_temp + (1|AN), data = mC2, family = binomial(link = "logit")),
+  lm(lmg_C2 ~ winAO + MEAN_temp + cumul_prec, data = mC2))
 # Get goodness-of-fit and AIC
-sem.fit(M3f6, f, conditional = T, corr.errors = "MEAN_temp ~~ cumul_prec")
+sem.fit(M3f6, mC2, conditional = T, corr.errors = "MEAN_temp ~~ cumul_prec")
 
 # No significant missing paths
-sem.coefs(M3f6, f)
-sem.plot(M3f6, f, show.nonsig = T)
+sem.coefs(M3f6, mC2)
+sem.plot(M3f6, mC2, show.nonsig = T)
+#Ceci est le dernier modele selectionne. Le probleme est que la variable pour les renards est mauvaise car il s'agit des abondances des tanieres repro et non pas des proportions. De plus il serait interessant de remplacer lmg_C2 par lmg_C1 ou lmg_C1_C2 histoire que ca corresponde plus avec la variable des renards.
 
-#### Path analysis with Path analysis_data 3bis #####
-#remplacement de lmg_abun par lmg_C1
+
+#### Même modele que pcdt en remplacant fox_dens par prop_fox_dens #####
 #remplacement de fox_dens par prop_fox_dens
 
-f2 <- f1[,-c(7,14,32,34,35)] #retrait de variables non necessaire afin d'eviter les NAs (ECLO, NEST.TYPE, lmg_C2, lmg_C1_C2, prim_prod)
+#Exploration visuelle des donnees
+X11()
+pairs(mC2[,c(16, 17, 20, 12, 4, 26)], upper.panel = panel.cor)
 
-M3f6autre<-list(
-  lm(prop_fox_dens~lmg_C1+winAO+cumul_prec+MEAN_temp, data = f2),
-  glmer(SN~prop_fox_dens+cumul_prec+MEAN_temp+ (1|AN), data=f2, family=binomial(link = "logit")),
-  lm(lmg_C1~winAO+MEAN_temp+cumul_prec, data = f2))
+#Modele de piste
+ro <- list(
+  lm(prop_fox_dens ~ lmg_C2 + winAO + cumul_prec + MEAN_temp, data = mC2),
+  glmer(SN ~ prop_fox_dens + cumul_prec + MEAN_temp + (1|AN), data = mC2, family = binomial(link = "logit")),
+  lm(lmg_C2 ~ winAO + MEAN_temp + cumul_prec, data = mC2))
 # Get goodness-of-fit and AIC
-sem.fit(M3f6autre, f2, conditional = T, corr.errors = "MEAN_temp ~~ cumul_prec")
+sem.fit(ro, mC2, conditional = T, corr.errors = "MEAN_temp ~~ cumul_prec")
 
-# No significant missing paths
-sem.coefs(M3f6autre, f2)
-sem.plot(M3f6autre, f2, show.nonsig = T)
+#Significant missing paths !!!!
+#sem.coefs(ro, mC2)
+#sem.plot(ro, mC2, show.nonsig = T)
+##### A RETRAVAILLER ####
 
-#### Path analysis with Path analysis_data 3bis #####
-#remplacement de lmg_abun par lmg_C2
-#remplacement de fox_dens par prop_fox_dens
+#### Même modele que pcdt en remplacant lmg_C2 par lmg_C1_C2 #####
 
-f3 <- f1[,-c(7,14,34,35)] #retrait de variables non necessaire afin d'eviter les NAs (ECLO, NEST.TYPE, lmg_C1_C2, prim_prod)
-f3 <- na.omit(f3)
+#Exploration visuelle des donnees
+X11()
+pairs(mC2[,c(16, 19, 20, 12, 4, 26)], upper.panel = panel.cor)
 
-M3f6autre2<-list(
-  lm(prop_fox_dens~lmg_C2+winAO+cumul_prec+MEAN_temp, data = f3),
-  glmer(SN~prop_fox_dens+cumul_prec+MEAN_temp+ (1|AN), data=f3, family=binomial(link = "logit")),
-  lm(lmg_C2~winAO+MEAN_temp+cumul_prec, data = f3))
+#Modele de piste
+ro1 <- list(
+  lm(prop_fox_dens ~ lmg_C1_C2 + winAO + cumul_prec + MEAN_temp, data = mC2),
+  glmer(SN ~ prop_fox_dens + cumul_prec + MEAN_temp + (1|AN), data = mC2, family = binomial(link = "logit")),
+  lm(lmg_C1_C2 ~ winAO + MEAN_temp + cumul_prec, data = mC2))
 # Get goodness-of-fit and AIC
-sem.fit(M3f6autre2, f3, conditional = T, corr.errors = "MEAN_temp ~~ cumul_prec")
+sem.fit(ro1, mC2, conditional = T, corr.errors = "MEAN_temp ~~ cumul_prec")
 
-# Significant missing paths
-sem.coefs(M3f6autre2, f3)
-sem.plot(M3f6autre2, f3, show.nonsig = T)
+#NO significant missing paths
+sem.coefs(ro1, mC2)
+sem.plot(ro1, mC2, show.nonsig = T)
+##### Modele valide mais refaire les etapes ulterieures ####
 
+#### Même modele que pcdt en remplacant lmg_C1_C2 par lmg_C1 #####
+#Changement de jeu de donnees. Utilisation de mC1 qui contient plus d'annees
 
-##### Exploration de f2 #####
-f2 <- f1[,-c(7,14,32,34,35)] #retrait de variables non necessaire afin d'eviter les NAs (ECLO, NEST.TYPE, lmg_C2, lmg_C1_C2, prim_prod)
+#Exploration visuelle des donnees
+X11()
+pairs(mC1[,c(15, 16, 17, 12, 4, 23)], upper.panel = panel.cor)
 
-# New initial modele
+#Modele de piste
+ro2 <- list(
+  lm(prop_fox_dens ~ lmg_C1 + winAO + cumul_prec + MEAN_temp, data = mC1),
+  glmer(SN ~ prop_fox_dens + cumul_prec + MEAN_temp + (1|AN), data = mC1, family = binomial(link = "logit")),
+  lm(lmg_C1 ~ winAO + MEAN_temp + cumul_prec, data = mC1))
+# Get goodness-of-fit and AIC
+sem.fit(ro2, mC1, conditional = T, corr.errors = "MEAN_temp ~~ cumul_prec")
+
+#NO significant missing paths
+sem.coefs(ro2, mC1)
+sem.plot(ro2, mC1, show.nonsig = T)
+##### Modele valide mais refaire les etapes ulterieures ####
+
+# RAPPEL
 #winAO = November to April
 #sprAO = 20 May to 20 June
 #esumAO = 21 June to 15 July
 #lsumAO = 16 July to 15 August
 #sumAO = 21 June to 15 August
 #AOnidif = 4 June to 19 July
-hope<-list(
-  lm(prop_fox_dens~lmg_C1+winAO+sprAO, data = f2),
-  glmer(SN~prop_fox_dens+cumul_prec+MEAN_temp+ (1|AN), data=f2, family=binomial(link = "logit")),
-  lm(lmg_C1~winAO+sprAO, data = f2),
-  lme(cumul_prec~AOnidif, data = f2, random = ~1|AN),
-  lme(MEAN_temp~AOnidif, data = f2, random = ~1|AN))
-# Get goodness-of-fit and AIC
-sem.fit(hope, f2, conditional = T)
-
-# No significant missing paths
-sem.coefs(hope, f2)
-sem.plot(hope, f2, show.nonsig = T)
