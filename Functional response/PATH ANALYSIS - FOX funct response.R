782c88d103604fcbf4e3436e66b6c849f443cee0
Your Name <nicolas@MacBook-Air-de-Nicolas-2.local>
2018-01-24 17:25:37 -0400

    Ajout de graphes

--- Functional response/PATH ANALYSIS - FOX funct response.R
+++ Functional response/PATH ANALYSIS - FOX funct response.R
@@ -17,16 +17,42 @@ require(piecewiseSEM)
 require(ggm)
 
 
-
+x11() #to open a graphic window
+#dev.set(dev.prev()) #to activate the graphic device when desactivated
 plot(fan$year,fan$atq_rate, ylab = "Attack rate (attack nb/second)", xlab = "year")
 plot(fan$winAO,fan$atq_rate, ylab = "Attack rate (attack nb/second)", xlab = "winter AO index")
 plot(fan$sprAO,fan$atq_rate, ylab = "Attack rate (attack nb/second)", xlab = "spring AO index")
-plot(fan$lmg_abun,fan$atq_rate, ylab = "Attack rate (attack nb/second)", xlab = "index of lemming abundance")
+plot(fan$lmg_C1,fan$atq_rate, ylab = "Attack rate (attack nb/second)", xlab = "index of lemming abundance C1")
+
+# Graphic functional response style
+plot(fan$lmg_C2, fan$atq_rate, ylab = "Attack rate (attack nb/second)", xlab = "index of lemming abundance C2")
+ajout <- with(fan, smooth.spline(lmg_C2,atq_rate))
+ajout
+lines(ajout, col = "orange")
+
+plot(fan$nest_density,fan$atq_rate, ylab = "Attack rate (attack nb/second)", xlab = "density of goose nests")
+ajout <- with(fan, smooth.spline(nest_density, atq_rate))
+ajout
+lines(ajout, col = "orange")
+
+plot(fan$lmg_C12,fan$atq_rate, ylab = "Attack rate (attack nb/second)", xlab = "index of lemming abundance C12")
 plot(fan$nest_density,fan$atq_rate, ylab = "Attack rate (attack nb/second)", xlab = "nest density of geese (nest nb/ha)")
+
 plot(fan$mean_temp,fan$atq_rate, ylab = "Attack rate (attack nb/second)", xlab = "mean temperature (c)")
+ajout_temp <- with(fan, smooth.spline(mean_temp,atq_rate))
+ajout_temp
+lines(ajout_temp, col = "orange")
+
 plot(fan$cumul_prec,fan$atq_rate, ylab = "Attack rate (attack nb/second)", xlab = "cumulative precipitation (mm)")
+ajout_prec <- with(fan, smooth.spline(cumul_prec,atq_rate))
+ajout_prec
+lines(ajout_prec, col = "pink")
+
 plot(fan$nest_succ,fan$atq_rate, ylab = "Attack rate (attack nb/second)", xlab = "nesting success of geese")
 
+boxplot(fan$atq_rate ~ fan$lmg_C2)
+boxplot(fan$atq_rate ~ fan$mean_temp)
+boxplot(fan$atq_rate ~ fan$cumul_prec)
 
 #### Modèle 1 ####
 names(fan)
